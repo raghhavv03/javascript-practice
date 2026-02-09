3b82d64d5762ca97b77829095f25c0e75df15663
Raghav Gupta <138666314+raghhavv03@users.noreply.github.com>
2026-02-09 03:24:25 -0500

    task-5 complete

---
+++ .history/task-5/js/bet_20260209031844.js
@@ -0,0 +1,17 @@
+const balance = Number(localStorage.getItem("balance"))
+document.getElementById("balance").textContent = balance
+
+const startGameBtn = document.getElementById("start-game-btn")
+const ErrorMsg = document.getElementById("bet-error-msg")
+
+startGameBtn.addEventListener("click", function() {
+    const bet = Number(document.getElementById("bet-input").value
+
+    if (bet <= 0 || bet > balance) {
+        ErrorMsg.textContent = "Please enter a valid bet amount"
+        return
+    }
+
+    localStorage.setItem("bet", bet)
+    window.location.href = "game.html"
+})
---
+++ .history/task-5/js/index_20260209032102.js
@@ -0,0 +1,15 @@
+const addMoneyBtn = document.getElementById("add-money-btn")
+const errorMsg = document.getElementById("index-error-msg")
+
+addMoneyBtn.addEventListener("click", function() {
+    const amount = Number(document.getElementById("money-input").value
+
+    if (amount <= 0) {
+        errorMsg.textContent = "Please enter a valid amount"
+        return
+    }
+
+    localStorage.setItem("balance", amount)
+    window.location.href = "bet.html"
+})
+
